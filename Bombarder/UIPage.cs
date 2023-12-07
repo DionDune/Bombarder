7a290fb0c2871d2257a18d7a551819163a02363e
DionDune <tomas.reynolds935@gmail.com>
2023-12-07 15:33:24 +0000

    Created the Pause Screen

--- Bombarder/UIPage.cs
+++ Bombarder/UIPage.cs
@@ -101,6 +101,87 @@ namespace Bombarder
                     }
                 }
             });
+            //Pause Page
+            Pages.Add(new UIPage()
+            {
+                Type = "Pause",
+
+                UIItems = new List<UIItem>()
+                {
+                    //Resume Button
+                    new UIItem()
+                    {
+                        Type = "Button",
+
+                        X = -200,
+                        Y = -75,
+
+                        Width = 400,
+                        Height = 150,
+
+                        CentreX = -200 + (400 / 2),
+                        CentreY = -75 + (150 / 2),
+
+                        BorderWidth = 5,
+                        BorderColor = Color.Green,
+                        BaseColor = Color.PaleGreen,
+
+                        Text = new TextElement()
+                        {
+                            Text = "RESUME",
+                            Elements = TextElement.GetString("RESUME"),
+                            ElementSize = 8,
+                            Color = Color.Black
+                        },
+
+                        Data = new List<string>() { "Resume" }
+                    },
+                    //Pause Quit Button
+                    new UIItem()
+                    {
+                        Type = "Button",
+
+                        X = -200,
+                        Y = 100,
+
+                        Width = 400,
+                        Height = 150,
+
+                        CentreX = -200 + (400 / 2),
+                        CentreY = 100 + (150 / 2),
+
+                        BorderWidth = 5,
+                        BorderColor = Color.DarkRed,
+                        BaseColor = Color.Red,
+
+                        Text = new TextElement()
+                        {
+                            Text = "QUIT",
+                            Elements = TextElement.GetString("QUIT"),
+                            ElementSize = 8,
+                            Color = Color.Black
+                        },
+
+                        Data = new List<string>() { "Quit" }
+                    },
+                    //Title Message
+                    new UIItem()
+                    {
+                        Type = "Text",
+                        X = 0,
+                        Y = 0,
+                        CentreX = 0,
+                        CentreY = -250,
+
+                        Text = new TextElement()
+                        {
+                            Elements = TextElement.GetString("BOMBARDER"),
+                            ElementSize = 16,
+                            Color = Color.White
+                        }
+                    }                
+                }
+            });
 
             //InGame
             Pages.Add(new UIPage()
