4311a3c9c2a1faf3a247c264c72fcd233477393f
TomasReynolds <Tomas.Reynolds935@gmail.com>
2023-12-11 15:17:25 +0000

    Added attributes to player classes, defined default attributes

--- Bombarder/Entity.cs
+++ Bombarder/Entity.cs
@@ -15,11 +15,39 @@ namespace Bombarder
         public float Direction { get; set; }
         public float BaseSpeed { get; set; }
 
+        bool ChasesPlayer;
+
         public List<EntityBlock> Peices { get; set; }
+
+        public Entity()
+        {
+            X = 0;
+            Y = 0;
+
+            Direction = 0;
+            BaseSpeed = 5;
+
+            ChasesPlayer = true;
+
+            Peices = new List<EntityBlock>() { new EntityBlock() };
+        }
     }
+
     internal class EntityBlock
     {
         public Vector2 Offset { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
         public Color Color { get; set; }
+
+        public EntityBlock()
+        {
+            Width = 16;
+            Height = 16;
+            Offset = new Vector2(-8, -8);
+
+            Color = Color.Purple;
+        }
     }
 }
