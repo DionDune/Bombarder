6b752b7935a14a414864956d1a2795ecfc35bcbd
TomasReynolds <Tomas.Reynolds935@gmail.com>
2023-12-11 15:12:31 +0000

    Created the Entity, Entity Block classes

---
+++ Bombarder/Entity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bombarder
+{
+    internal class Entity
+    {
+        public float X {  get; set; }
+        public float Y { get; set; }
+
+        public float Direction { get; set; }
+        public float BaseSpeed { get; set; }
+
+        public List<EntityBlock> Peices { get; set; }
+    }
+    internal class EntityBlock
+    {
+        public Vector2 Offset { get; set; }
+        public Color Color { get; set; }
+    }
+}
