6e42751153cf5c0dc2a33653eeae4244f52113df
TomasReynolds <Tomas.Reynolds935@gmail.com>
2023-12-12 13:07:45 +0000

    Created the Settings class

---
+++ Bombarder/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bombarder
+{
+    internal class Settings
+    {
+        public bool ShowGrid { get; set; }
+        public bool ShowDamageRadii { get; set; }
+        public bool RunEntityAI { get; set; }
+
+        public Settings()
+        {
+            ShowGrid = true;
+            ShowDamageRadii = true;
+            RunEntityAI = true;
+        }
+    }
+}
