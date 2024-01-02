757eec41379174a982e68c5dd1c7a6ed9062e0c7
DionDune <Tomas.Reynolds935@Gmail.com>
2024-01-02 14:13:52 +0000

    Added HealthBarDimentions attribute to Entity

--- Bombarder/Entity.cs
+++ Bombarder/Entity.cs
@@ -17,6 +17,7 @@ namespace Bombarder
         public int HealthMax { get; set; }
         public bool IsDead { get; set; }
 
+        public Point HealthBarDimentions { get; set; }
         public Point HealthBarOffset { get; set; }
         public bool HealthBarVisible { get; set; }
 
