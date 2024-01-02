6735dcb096691196671d39bd0ff56978115b34b8
DionDune <Tomas.Reynolds935@Gmail.com>
2024-01-02 12:48:39 +0000

    Created the InputStates Class

---
+++ Bombarder/InputStates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+namespace Bombarder
+{
+    internal class InputStates
+    {
+        public List<Keys> PreviouseKeys { get; set; }
+        public bool isClickingLeft { get; set; }
+        public bool isClickingRight { get; set; }
+        public bool isClickingMiddle { get; set; }
+
+        public InputStates()
+        {
+            PreviouseKeys = new List<Keys>();
+
+            isClickingLeft = false;
+            isClickingRight = false;
+            isClickingMiddle = false;
+        }
+    }
+}
