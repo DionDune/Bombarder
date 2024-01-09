568084ffe007e1142ce0c69d5b68dfa1ce94ed20
DionDune <tomas.reynolds935@gmail.com>
2024-01-09 23:38:48 +0000

    Created the Object class

---
+++ Bombarder/Objects.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Bombarder
+{
+    internal class Object
+    {
+        public string Type { get; set; }
+
+        public bool HasDuration { get; set; }
+        public int Duration { get; set; }
+
+        public int TextureTag { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
